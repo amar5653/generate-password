24031671260d8d02df05416384be97bff73ecfd9
Amarpreet <gillpreet1998@gmail.com>
2024-01-01 21:06:09 -0700

    script.js file

---
+++ script.js
@@ -0,0 +1,28 @@
+const passwordBox = document.getElementById("password");
+let length = 14;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase ="abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*";
+
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPassword(){
+   let password = "";
+   password += upperCase[Math.floor(Math.random() * upperCase.length)];
+   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+   password += numbers[Math.floor(Math.random() * numbers.length)];
+   password += symbols[Math.floor(Math.random() * symbols.length)];
+
+   while(password.length < length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+   }
+
+   passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.Select();
+    document.execCommand("copy");
+}
